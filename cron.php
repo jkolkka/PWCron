6a736000cd1331bdd8fd7112e285cb041d9543ca
Joani Eizmendi <theloghownothing@gmail.com>
2017-02-06 19:11:05 +0100

    fix

--- cron.php
+++ cron.php
@@ -1,21 +1,22 @@
-<?php
-/**
- * Cron file to call from /etc/crontab
- *
- * Call this file from system crontab
- *
- * example:
- * 0,15,30,45 * * * * user  php <path_to_pw>/site/modules/PWCron/cron.php
- *
- * @author Joonas Kolkka <joonas.kolkka@comspot.fi>
- * @author Ville Sulin <ville.sulin@comspot.fi>
- */
+<?php namespace ProcessWire;
+    /**
+     * Cron file to call from /etc/crontab
+     *
+     * Call this file from system crontab
+     *
+     * example:
+     * 0,15,30,45 * * * * user  php <path_to_pw>/site/modules/PwCron/cron.php
+     *
+     * @author Joonas Kolkka <joonas.kolkka@comspot.fi>
+     * @author Ville Sulin <ville.sulin@comspot.fi>
+     */
 
 /** Bootstrap ProcessWire */
 require_once(__DIR__ . '/../../../index.php');
 
-$cron = wire('modules')->get('PWCron');
-
-if($cron) {
-  $cron->run();
+if($wire->modules->isInstalled('PwCron')) {
+    $cron = $wire->modules->get('PwCron');
+    if($cron) {
+        $cron->run();
+    }
 }
